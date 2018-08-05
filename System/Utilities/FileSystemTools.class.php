d5b27afecd02eccedd8428fd3a11aa6dfbc6a832
Jon Hulka <jon.hulka@gmail.com>
2018-08-05 14:34:03 -0700

    Converted ZedBoot\System\Utilities\FileSystemTools to exception based error handling
    Signed-off-by: Jon Hulka <jon.hulka@gmail.com>

--- System/Utilities/FileSystemTools.class.php
+++ System/Utilities/FileSystemTools.class.php
@@ -1,13 +1,13 @@
 <?php
 namespace ZedBoot\System\Utilities;
-class FileSystemTools implements \ZedBoot\System\Error\ErrorReporterInterface
+use \ZedBoot\System\Error\ZBError as Err;
+class FileSystemTools
 {
 	protected
 		$safePath=null;
-	public function getError(){ return $this->errorLogger->getUserMessage(); }
 	public function __construct($safePath)
 	{
-		if(empty($safePath)) throw new \Exception('Empty safe path.');
+		if(empty($safePath)) throw new Err('Empty safe path.');
 		$this->safePath=$safePath;
 	}
 	/**
@@ -15,25 +15,10 @@ class FileSystemTools implements \ZedBoot\System\Error\ErrorReporterInterface
 	 */
 	public function rmdirRecursive($path)
 	{
-		$result=false;
-		try
-		{
-			$this->rmr($path);
-			$result=true;
-		}
-		catch(\Exception $e)
-		{
-			error_log($e);
-			$this->error=$e->getMessage();
-		}
-		return $result;
-	}
-	protected function rmr($path)
-	{
 		$subs=null;
-		if(empty($path) || !is_dir($path)) throw new \Exception('Attempt to remove non-existent directory '.$path);
-		if(substr($path,0,strlen($this->safePath))!==$this->safePath) throw new \Exception('Attempt to remove directory not within the safe path: '.$path.' (safe path is '.$this->safePath.').');
-		if(false===($subs=glob($path.'/{,.}[!.,!..]*', GLOB_BRACE))) throw new \Exception('System error: Unable to get directory contents for '.$path);
+		if(empty($path) || !is_dir($path)) throw new Err('Attempt to remove non-existent directory '.$path);
+		if(substr($path,0,strlen($this->safePath))!==$this->safePath) throw new Err('Attempt to remove directory not within the safe path: '.$path.' (safe path is '.$this->safePath.').');
+		if(false===($subs=glob($path.'/{,.}[!.,!..]*', GLOB_BRACE))) throw new Err('System error: Unable to get directory contents for '.$path);
 		//Excluding . and .. is redundant, but a good idea for safety
 		foreach($subs as $sub) if($sub!='.' && $sub!='..')
 		{
@@ -41,8 +26,8 @@ class FileSystemTools implements \ZedBoot\System\Error\ErrorReporterInterface
 			{
 				$this->rmr($sub);
 			}
-			else if(is_file($sub) && !unlink($sub)) throw new \Exception('System error: Unable to delete file '.$sub);
+			else if(is_file($sub) && !unlink($sub)) throw new Err('System error: Unable to delete file '.$sub);
 		}
-		if(!rmdir($path)) throw new \Exception('Unable to delete directory '.$path);
+		if(!rmdir($path)) throw new Err('Unable to delete directory '.$path);
 	}
 }
