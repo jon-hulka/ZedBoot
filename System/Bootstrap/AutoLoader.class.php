3ce24133a30d9daf951cb84309d0de4f28baf309
Jonathan Hulka <jon@speedduck.localdomain>
2018-12-09 14:06:10 -0800

    Modified class autoloader to allow more flexible file naming
    Signed-off-by: Jonathan Hulka <jon@speedduck.localdomain>

--- System/Bootstrap/AutoLoader.class.php
+++ System/Bootstrap/AutoLoader.class.php
@@ -11,13 +11,29 @@ namespace ZedBoot\System\Bootstrap;
 
 /**
  * Simple class loader
- * Namespaces are mapped to directories. Class files are expected to have a .class.php extension.
+ * Namespaces are mapped to directories.
  */
 class AutoLoader
 {
 	protected
-		//Nested array of namespaces and paths - the 'path' element is optional at any level
-		//array('NS'=>array('subs'=>array('NSOTHER'=>array('subs'=>array(...),'path'=>'/another_path/),'NSTHAT'=>array(...)),'path'='/path'))
+		//Set up by findPath
+		$suffix,
+		//Set up by findPath
+		$prefix,
+		//Nested array of namespaces and paths - the 'path' element indicates that that level is mapped
+		//array(
+		//	'NS'=>array( // \NS is not mapped to any path, it is here because sub-namespaces are mapped
+		//		'subs'=>array(
+		//			'NSOTHER'=>array( // \NS\NSOTHER is mapped to /another_path
+		//				'subs'=>array(...),
+		//				'path'=>'/another_path',
+		//				'suffix'=>'.class.php', //default prefix and suffix for class file names
+		//				'prefix'=>''
+		//			),
+		//			'NSTHAT'=>array(...) //And so on...
+		//		)
+		//	)
+		//)
 		$namespaces=array();
 	public function __construct()
 	{
@@ -35,13 +51,16 @@ class AutoLoader
 	 * Sub-namespaces will be loaded as subdirectories, unless explicitly mapped.
 	 * @param string $namespace leading and trailing backslashes are unnecessary
 	 * @param string $path must be the absolute path
+	 * @param string $suffix part of file name following the class name (normally the extension '.class.php')
+	 * @param string $prefix part of file name preceding the class name (normally empty)
 	 * @return void
 	 */
-	public function register($namespace,$path)
+	public function register($namespace,$path,$suffix='.class.php',$prefix='')
 	{
 		if(!empty($namespace))
 		{
 			$index=&$this->namespaces;
+			//Break the namespace into segments
 			$parts=explode('\\',trim($namespace,'\\'));
 			$partIndex=null;
 			while(!is_null($part=array_shift($parts)))
@@ -50,7 +69,12 @@ class AutoLoader
 				$partIndex=&$index[$part];
 				$index=&$index[$part]['subs'];
 			}
-			if(is_array($partIndex)) $partIndex['path']=$path;
+			if(is_array($partIndex))
+			{
+				$partIndex['path']=$path;
+				$partIndex['suffix']=$suffix;
+				$partIndex['prefix']=$prefix;
+			}
 		}
 	}
 	/**
@@ -72,8 +96,8 @@ class AutoLoader
 			{
 				//The remaining elements of $parts are subdirectories of the namespace top level directory
 				//Put the class name back
-				$parts[]=$className;
-				$result.=DIRECTORY_SEPARATOR.implode(DIRECTORY_SEPARATOR, $parts).'.class.php';
+				$parts[]=$this->prefix.$className.$this->suffix;
+				$result.=DIRECTORY_SEPARATOR.implode(DIRECTORY_SEPARATOR, $parts);
 			}
 		}
 		return $result;
@@ -82,7 +106,8 @@ class AutoLoader
 	/**
 	 * Helper function for getPath
 	 * Recursively searches and matches a namespace with its path. getPath() does pre and post processing
-	 * @param array $parts namespace segments, not including path. After a namespace is found, only its sub-namespaces will be left in this parameter.
+	 * $this->suffix and $this->prefix will be modified on successful search
+	 * @param array $parts namespace segments, not including path. After a namespace is found, only its sub-namespaces will be left.
 	 * @param array $index namespace index - see $this->namespaces for structure
 	 * @param string|null $path path at current level of recursion
 	 * @return string|null result path or null if none found
@@ -94,7 +119,19 @@ class AutoLoader
 		{
 			//Try the next level in
 			$part=array_shift($parts);
-			if(array_key_exists($part,$index)) $result=$this->findPath($parts,$index[$part]['subs'],empty($index[$part]['path'])?null:$index[$part]['path']);
+			if(array_key_exists($part,$index))
+			{
+				$p=null;
+				if(array_key_exists('path',$index[$part]))
+				{
+					//Presence of 'path' attribute indicates namespace registered at this level
+					$p=$index[$part]['path'];
+					//Get prefix and suffix out to top level of recursion
+					$this->suffix=$index[$part]['suffix'];
+					$this->prefix=$index[$part]['prefix'];
+				}
+				$result=$this->findPath($parts,$index[$part]['subs'],$p);
+			}
 			if(is_null($result))
 				//Not found - rewind to next level out
 				array_unshift($parts,$part);
