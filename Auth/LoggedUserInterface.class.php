24d19b45b425f43ad47e4aaab1e15be608ee4ff4
Jonathan Hulka <jon@speedduck.localdomain>
2020-02-05 09:28:20 -0800

    Simplifications to LoggedUserInterface
    Signed-off-by: Jonathan Hulka <jon.hulka@gmail.com>

--- Auth/LoggedUserInterface.class.php
+++ Auth/LoggedUserInterface.class.php
@@ -4,7 +4,7 @@
  * @license     GNU General Public License, version 3
  * @package     Auth
  * @author      Jonathan Hulka <jon.hulka@gmail.com>
- * @copyright   Copyright (c) 2017-2018 Jonathan Hulka
+ * @copyright   Copyright (c) 2017 - 2020 Jonathan Hulka
  */
 
 /**
@@ -15,32 +15,16 @@ namespace ZedBoot\Auth;
 interface LoggedUserInterface
 {
 	/**
-	 * In case of failure, returns user-friendly message.
-	 * @return mixed user friendly message from last failure, if any.
-	 */
-	public function getMessage();
-	/**
 	 * Should implicitly load data if not loaded
-	 * @return mixed null if no user logged in, false on failure, otherwise Array('id'=><user id>,'username'=><user name>,'info'=><info>,'roles'=><roles>,'modTime'=><mod time>,'loginTime'=><login time>)
-	 * modTime and loginTime should be unix timestamps (expressed in seconds, decimals are OK)
-	 * modTime indicates last time the user was modified (if there are no recent changes (since login time), a value of 0 is acceptable)
+	 * @return mixed null if no user logged in, user data if a user is logged in
 	 */
 	public function getUser();
 	/**
 	 * Sets user for the session
 	 * @param String $id
-	 * @param String $username
-	 * @param Array $info miscellaneous user data
-	 * @param Array $roles roles assigned to the user
-	 * @param Numeric $modTime last time the user was modified
-	 * @return boolean true on success, false on failure
 	 */
 	public function setUser(
-		$id,
-		$username,
-		Array $info,
-		Array $roles,
-		$modTime);
+		string $id);
 	/**
 	 * Clears user for the session
 	 */
