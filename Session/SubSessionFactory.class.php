3896fb1a62dca609148f0cedf0bd1d95f76c4c8b
Jonathan Hulka <jon@speedduck.localdomain>
2020-07-26 21:41:29 -0700

    Added expiry to SubSessionFactory constructor.
    Signed-off-by: Jonathan Hulka <jon.hulka@gmail.com>

--- Session/SubSessionFactory.class.php
+++ Session/SubSessionFactory.class.php
@@ -4,14 +4,13 @@
  * @license     GNU General Public License, version 3
  * @package     Session
  * @author      Jonathan Hulka <jon.hulka@gmail.com>
- * @copyright   Copyright (c) 2019 Jonathan Hulka
+ * @copyright   Copyright (c) 2019 - 2020 Jonathan Hulka
  */
 
 /**
  * SessionFactoryInterface implementation
- * Decorates a SessionFactoryInterface instance. Multiple
- * SubSessionFactories can share a single SessionFactoryInterface 
- * without risk of key conflicts.
+ * Decorates a SessionFactoryInterface instance.
+ * Multiple SubSessionFactories can share a single SessionFactoryInterface without risk of key conflicts.
  */
 
 namespace ZedBoot\Session;
@@ -19,19 +18,28 @@ class SubSessionFactory implements \ZedBoot\Session\SessionFactoryInterface
 {
 	protected
 		$sessionFactory,
-		$subPath;
+		$subPath,
+		$expiry = null;
+
 	/**
-	 * @param $session \ZedBoot\Session\SessionInterface
-	 * @param $subPath String namespace to be used for generated SubSessions
+	 * @param \ZedBoot\Session\SessionInterface $session parent session
+	 * @param string $subPath namespace to be used for generated SubSessions
+	 * @param $expiry int|null optional expiry in seconds, if null parent session expiry will be used, if 0 no expiry
 	 */
-	public function __construct(\ZedBoot\Session\SessionFactoryInterface $sessionFactory,$subPath)
+	public function __construct
+	(
+		\ZedBoot\Session\SessionFactoryInterface $sessionFactory,
+		string $subPath,
+		int $expiry = null
+	)
 	{
 		$this->sessionFactory=$sessionFactory;
 		$this->subPath=$subPath;
+		$this->expiry = $expiry;
 	}
 	
 	public function getSession($sessionId)
 	{
-		return new \ZedBoot\Session\SubSession($this->sessionFactory->getSession($sessionId),$this->subPath);
+		return new \ZedBoot\Session\SubSession($this->sessionFactory->getSession($sessionId), $this->subPath, $this->expiry);
 	}
 }
