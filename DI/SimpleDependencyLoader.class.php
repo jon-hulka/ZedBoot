2e473ee6bee7456e470c7666a0590956168be834
Jon Hulka <jon.hulka@gmail.com>
2021-12-01 18:35:56 +0000

    Cleaned up code and improved dependency loading errors to be more descriptive.

--- DI/SimpleDependencyLoader.class.php
+++ DI/SimpleDependencyLoader.class.php
@@ -16,24 +16,6 @@ namespace ZedBoot\DI;
 use \ZedBoot\Error\ZBError as Err;
 class SimpleDependencyLoader implements \ZedBoot\DI\DependencyLoaderInterface
 {
-/*
- * 2020-04-09 This appears to be unused - leaving for now in case I am mistaken
-	protected static
-		//Expected results from gettype
-		//Searching is faster on keys rather than values
-		$types=[
-			'boolean'=>true,
-			'integer'=>true,
-			'double'=>true,
-			'string'=>true,
-			'array'=>true,
-			'object'=>true,
-			'resource'=>true,
-			'resource (closed)'=>true,
-			'NULL'=>true,
-			'unknown type'=>true
-		];
-*/
 	protected
 		$dependencyIndex=null,
 		$singletons=[];
@@ -41,15 +23,15 @@ class SimpleDependencyLoader implements \ZedBoot\DI\DependencyLoaderInterface
 	{
 		$this->dependencyIndex=$dependencyIndex;
 	}
-	public function getDependency(String $id,String $type=null)
+	public function getDependency(string $id, string $type=null)
 	{
-		$result=$this->loadDependency($id,[]);
+		$result = $this->loadDependency($id, []);
 		if(!empty($type))
 		{
-			$t=gettype($result);
-			if($t!==$type)
+			$t = gettype($result);
+			if($t !== $type)
 			{
-				if($t==='object')
+				if($t === 'object')
 				{
 					if(!($result instanceof $type)) throw new Err('Getting '.$id.': Expected '.$type.', got '.get_class($result));
 				}
@@ -58,9 +40,9 @@ class SimpleDependencyLoader implements \ZedBoot\DI\DependencyLoaderInterface
 		}
 		return $result;
 	}
-	protected function loadDependency(String $id,array $dependencyChain)
+	protected function loadDependency(string $id, array $dependencyChain)
 	{
-		$result=false;
+		$result = false;
 		try
 		{
 			if(array_key_exists($id, $this->singletons))
@@ -121,7 +103,7 @@ class SimpleDependencyLoader implements \ZedBoot\DI\DependencyLoaderInterface
 	{
 		//Make sure this alias is not its own ancestor
 		if(false !== (array_search($id, $dependencyChain, true)))
-			throw new Err('Circular dependency: '.implode(' > ',$dependencyChain).' > '.$id);
+			throw new Err('Circular dependency: '.implode(' > ', $dependencyChain).' > '.$id);
 		$dependencyChain[] = $id;
 		return $this->loadDependency($def['alias_of_id'], $dependencyChain);
 	}
@@ -131,11 +113,11 @@ class SimpleDependencyLoader implements \ZedBoot\DI\DependencyLoaderInterface
 	 */
 	protected function loadArrayElement(string $id, array $def, array $dependencyChain)
 	{
-		$result=null;
+		$result = null;
 		$ne = $def['if_not_exists'];
 		//Make sure this array is not its own ancestor
-		if(false!==(array_search($id, $dependencyChain, true)))
-			throw new Err('Circular dependency: '.implode(' > ',$dependencyChain).' > '.$id);
+		if(false !== (array_search($id, $dependencyChain, true)))
+			throw new Err('Circular dependency: '.implode(' > ', $dependencyChain).' > '.$id);
 		$dependencyChain[] = $id;
 		$arr = $this->loadDependency($def['array_id'], $dependencyChain);
 		if(!is_array($arr)) throw new Err($id.' arrayId: Expected '.$def['array_id'].' to be array, got '.gettype($arr));
@@ -163,30 +145,30 @@ class SimpleDependencyLoader implements \ZedBoot\DI\DependencyLoaderInterface
 	 */
 	protected function loadObjectProperty(string $id, array $def, array $dependencyChain)
 	{
-		$result=null;
-		$ne=$def['if_not_exists'];
+		$result = null;
+		$ne = $def['if_not_exists'];
 		//Make sure this object is not its own ancestor
-		if(false!==(array_search($id,$dependencyChain,true)))
-			throw new Err('Circular dependency: '.implode(' > ',$dependencyChain).' > '.$id);
-		$dependencyChain[]=$id;
-		$obj=$this->loadDependency($def['object_id'], $dependencyChain);
+		if(false !== (array_search($id, $dependencyChain, true)))
+			throw new Err('Circular dependency: '.implode(' > ', $dependencyChain).' > '.$id);
+		$dependencyChain[] = $id;
+		$obj = $this->loadDependency($def['object_id'], $dependencyChain);
 		if(!is_object($obj)) throw new Err($id.' objectId: Expected '.$def['object_id'].' to be object, got '.gettype($obj));
-		$prop=$def['property'];
-		if(property_exists($obj,$prop))
+		$prop = $def['property'];
+		if(property_exists($obj, $prop))
 		{
-			$result=$obj->$prop;
+			$result = $obj->$prop;
 		}
 		else if(is_string($ne))
 		{
-			$result=$this->loadDependency($ne, $dependencyChain);
+			$result = $this->loadDependency($ne, $dependencyChain);
 		}
 		else if(is_array($ne))
 		{
-			$result=$this->extractArguments($ne,$dependencyChain,true,'array element');
+			$result = $this->extractArguments($ne, $dependencyChain, true, 'array element');
 		}
 		else
 		{
-			$result=$ne;
+			$result = $ne;
 		}
 		return $result;
 	}
@@ -195,68 +177,68 @@ class SimpleDependencyLoader implements \ZedBoot\DI\DependencyLoaderInterface
 	 * @param $id
 	 * @param array $dependencyChain dependencies for the current branch of the dependency tree. Used to detect circular dependencies.
 	 */
-	protected function loadService(String $id,array $def,array $dependencyChain)
+	protected function loadService(string $id, array $def, array $dependencyChain)
 	{
-		$result=false;
-		$argValues=[];
+		$result = false;
+		$argValues = [];
 		//Make sure this service is not its own ancestor
-		if(false!==(array_search($id,$dependencyChain,true)))
-			throw new Err('Circular dependency: '.implode(' > ',$dependencyChain).' > '.$id);
+		if(false !== (array_search($id, $dependencyChain, true)))
+			throw new Err('Circular dependency: '.implode(' > ', $dependencyChain).' > '.$id);
 		//Retrieve arguments needed by this service's constructor
-		$dependencyChain[]=$id;
-		$argValues=$this->extractArguments($def['args'],$dependencyChain);
+		$dependencyChain[] = $id;
+		$argValues = $this->extractArguments($def['args'], $dependencyChain);
 		//Create a new instance
-		$cn=$def['class_name'];
+		$cn = $def['class_name'];
 		try
 		{
-			$reflect=new \ReflectionClass($cn);
-			$result=$reflect->newInstanceArgs($argValues);
+			$reflect = new \ReflectionClass($cn);
+			$result = $reflect->newInstanceArgs($argValues);
 		}
 		catch(\Exception $e)
 		{
-			throw new Err('Loading dependency: '.$e->getMessage().': Dependency chain: '.implode(' > ', $dependencyChain));
+			throw new Err('Loading dependency (instance of '.$cn.'): '.$e->getMessage().': Dependency chain: '.implode(' > ', $dependencyChain));
 		}
-		if(is_object($result)) $this->checkSetterInjection($result,$id);
-		if($def['singleton']) $this->singletons[$id]=$result;
+		if(is_object($result)) $this->checkSetterInjection($result, $id);
+		if($def['singleton']) $this->singletons[$id] = $result;
 		return $result;
 	}
 
-	protected function loadFactoryService(String $id,array $def,array $dependencyChain)
+	protected function loadFactoryService(string $id, array $def, array $dependencyChain)
 	{
-		$result=false;
-		$factory=null;
-		$argValues=[];
+		$result = false;
+		$factory = null;
+		$argValues = [];
 		//Make sure this dependency is not its own ancestor
-		if(false!==(array_search($id,$dependencyChain,true)))
-			throw new Err('Circular dependency: '.implode(' > ',$dependencyChain).' > '.$id);
-		$dependencyChain[]=$id;
-		$factoryId=$def['factory_id'];
-		if(false!==(array_search($factoryId,$dependencyChain,true)))
-			throw new Err('Circular dependency: '.implode(' > ',$dependencyChain).' > '.$factoryId);
-		$factory=$this->loadDependency($factoryId,$dependencyChain);
+		if(false !== (array_search($id, $dependencyChain, true)))
+			throw new Err('Circular dependency: '.implode(' > ', $dependencyChain).' > '.$id);
+		$dependencyChain[] = $id;
+		$factoryId = $def['factory_id'];
+		if(false !== (array_search($factoryId, $dependencyChain, true)))
+			throw new Err('Circular dependency: '.implode(' > ', $dependencyChain).' > '.$factoryId);
+		$factory = $this->loadDependency($factoryId, $dependencyChain);
 		if(!is_object($factory)) throw new Err('Expected factory '.json_encode($factoryId).' to be an object. Got '.gettype($factory).': Dependency chain: '.implode(' > ', $dependencyChain));
-		$argValues=$this->extractArguments($def['args'],$dependencyChain);
+		$argValues = $this->extractArguments($def['args'], $dependencyChain);
 		try
 		{
-			$result=([$factory,$def['function']])(...$argValues);
+			$result = ([$factory, $def['function']])(...$argValues);
 		}
 		catch(\Exception $e)
 		{
 			throw new Err('Running factory function: '.$id.': '.$factoryId.'::'.$def['function'].'(): '.$e->getMessage().': Dependency chain: '.implode(' > ', $dependencyChain));
 		}
-		if(is_object($result)) $this->checkSetterInjection($result,$id);
-		if($def['singleton']) $this->singletons[$id]=$result;
+		if(is_object($result)) $this->checkSetterInjection($result, $id);
+		if($def['singleton']) $this->singletons[$id] = $result;
 		return $result;
 
 	}
 
-	protected function checkSetterInjection($service,String $id)
+	protected function checkSetterInjection($service, string $id)
 	{
-		$setterInjections=$this->dependencyIndex->getSetterInjections($id);
+		$setterInjections = $this->dependencyIndex->getSetterInjections($id);
 		foreach($setterInjections as $def)
 		{
-			$argValues=$this->extractArguments($def['args'],[]);
-			([$service,$def['function']])(...$argValues);
+			$argValues = $this->extractArguments($def['args'], []);
+			([$service, $def['function']])(...$argValues);
 		}
 	}
 
